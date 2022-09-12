a5ac5a20bbcdecf3da936b46a53d99bd3f74c53b
almahfuz <almahfuz0327@gmail.com>
2022-09-12 10:31:30 +0600

    first commit

---
+++ src/Components/Card.js
@@ -0,0 +1,18 @@
+import React from "react"
+import self2 from "../Images/self2.JPG"
+
+function Card() {
+    return (
+        <section className="head">
+            <div>
+                <img className="head-pic" src={self2} alt="header picture"></img>
+            </div>
+            <div>
+
+            </div>
+
+        </section>
+    )
+}
+
+export default Card;
